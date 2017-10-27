3955e7a08975f00818b4986ea5a1ae11610bb288
baskar <baskar10576@gmail.com>
2017-10-27 16:11:11 +0530

    report path added.

--- com.selenium.example.demo/src/test/java/com/cucumber/runner/ChromeRunner.java
+++ com.selenium.example.demo/src/test/java/com/cucumber/runner/ChromeRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = true
 		,features = {"src/test/resources/SearchAmazonProducts.feature"}
-		,plugin = {"pretty:STDOUT","html:D:\\Baskar\\Workspace\\TestData\\eclipse\\workspace\\cucumber-pretty"}
+		,plugin = {"pretty:STDOUT","html:C:\\Users\\616181\\git\\Testleaf1\\com.selenium.example.demo\\cucumber-pretty"}
 		,monochrome = true
 		,glue = {"com.selenium.example.demo"}
 		)
